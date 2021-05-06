a665b73b41f62b017139c80a9c0e45cdee4ae6e4
TWC58 <47580948+TWC58@users.noreply.github.com>
2021-05-06 18:41:46 -0400

    updated directory structure to function on my computer(?)

---
+++ wearever/myapp/views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from django.template.loader import render_to_string
+from django.conf.urls import url
+
+# Create your views here.
+def home(request):
+    title='Wearever'
+    html = render_to_string('home.html', {'title': 'Wearever'})
+    return HttpResponse(html)
+
+def about(request):
+    group = 'Inverse Intelligence'
+    title='Wearever'
+    html = render_to_string('about.html', {'title': title, 'group': group})
+    return HttpResponse(html)
