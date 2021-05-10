13df1119dd293e2e823a2238ce7d0a39756e7be5
srindrakanti <srindrakanti@users.noreply.github.com>
2021-05-10 14:57:48 -0400

    set up database model

--- wearever/myapp/models.py
+++ wearever/myapp/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class Recommendation(models.Model):
+    email = models.EmailField()
+    temp = models.FloatField()
+    humidity = models.IntegerField()
+    windspeed = models.FloatField()
+    date = models.DateField(auto_now_add=True)
+    rec = models.JSONField()
+
+    def __str__(self):
+        return '{} {}: {}'.format(self.email, self.date, self.rec)
