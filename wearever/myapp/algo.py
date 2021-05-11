9d69e6c31b3e89b8f092c4870bab2f28daa23df9
Aneek Barua <baruaaneek@gmail.com>
2021-05-10 20:29:23 -0400

    Algo implementation with multi SGD

--- wearever/myapp/algo.py
+++ wearever/myapp/algo.py
@@ -1,6 +1,19 @@
 from sklearn.linear_model import SGDClassifier
+import sqlite3
 
-X = [[0., 0.], [1., 1.]]
-y = [0, 1]
 clf = SGDClassifier(loss="hinge", penalty="l2", max_iter=200)
-print(clf.fit(X, y))
+clf = SGDClassifier(loss="hinge", penalty="l2", max_iter=200)
+
+top_list = ["raincoat", "hoodie", "sweatshirt", "short sleeve", "long sleeve", "winter jacket"]
+bottom_list = ["pants", "sweatpants", "shorts"]
+footwear_list = ["boots", "sneakers", "sandals"]
+end_ind = 0
+
+train_set = []
+train_labels = []
+connect = sqlite3.connect('db.sqlite3')
+cursor = connect.cursor
+for row in cursor.execute("SELECT * FROM "):
+    train_set.append(list(row[:end_ind]))
+    train_labels.append(list(row[end_ind:]))
+
