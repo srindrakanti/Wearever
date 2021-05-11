f0cab2ee2841826fb8b28af409774b6ade6e43a6
TWC58 <47580948+TWC58@users.noreply.github.com>
2021-05-10 20:15:37 -0400

    fixed import issue,heat index calc

--- wearever/myapp/models.py
+++ wearever/myapp/models.py
@@ -11,3 +11,9 @@ class Recommendation(models.Model):
 
     def __str__(self):
         return '{} {}: {}'.format(self.email, self.date, self.rec)
+
+    #def get_last_rec(self): returns last set of recommended clothing with date in dictionary as {'toprec': X, 'bottomrec': X, 'footwearrec': X, 'layersrec': X, 'date': X}
+
+    #def save_feedback(self, top, bottom, footwear, layers, date): saves feedback into database for date, overrides if feedback already exists for that date (maybe return weather too?)
+
+    #def get_rec(self, temp, humi, wind, user='default'): returns dictionary with recommendations as previously described (no date), if user not specified give default(for non-logged in home page)
