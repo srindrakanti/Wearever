2f80b2aba337a6c8c060ed780fe4f7734947dc22
Aneek Barua <baruaaneek@gmail.com>
2021-05-10 20:31:10 -0400

    new algo again

--- wearever/myapp/algo.py
+++ wearever/myapp/algo.py
@@ -1,8 +1,10 @@
 from sklearn.linear_model import SGDClassifier
 import sqlite3
 
-clf = SGDClassifier(loss="hinge", penalty="l2", max_iter=200)
-clf = SGDClassifier(loss="hinge", penalty="l2", max_iter=200)
+clf_top = SGDClassifier(loss="hinge", penalty="l2", max_iter=200)
+clf_bottom = SGDClassifier(loss="hinge", penalty="l2", max_iter=200)
+clf_footwear = SGDClassifier(loss="hinge", penalty="l2", max_iter=200)
+clf_layers = SGDClassifier(loss="hinge", penalty="l2", max_iter=200)
 
 top_list = ["raincoat", "hoodie", "sweatshirt", "short sleeve", "long sleeve", "winter jacket"]
 bottom_list = ["pants", "sweatpants", "shorts"]
@@ -16,4 +18,9 @@ cursor = connect.cursor
 for row in cursor.execute("SELECT * FROM "):
     train_set.append(list(row[:end_ind]))
     train_labels.append(list(row[end_ind:]))
+
+clf_top.fit(train_set, train_labels[:][0])
+clf_bottom.fit(train_set, train_labels[:][1])
+clf_top.fit(train_set, train_labels[:][2])
+clf_bottom.fit(train_set, train_labels[:][3])
 
