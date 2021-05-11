132d0041ff2fffed033d6dc6b1e3bedd27d17885
srindrakanti <srindrakanti@users.noreply.github.com>
2021-05-11 00:50:59 -0400

    finished algorithm and CRUD functionality

--- wearever/myapp/views.py
+++ wearever/myapp/views.py
@@ -6,6 +6,8 @@ from pyowm import OWM
 from pyowm.utils import config
 from pyowm.utils import timestamps
 from django.views.decorators.csrf import csrf_exempt
+from myapp.models import Recommendation
+from myapp.algo import get_rec
 
 # Create your views here.
 def home(request):
@@ -15,8 +17,7 @@ def home(request):
     observation = mgr.weather_at_place('Stony Brook, US')
     w = observation.weather
     #SET DEFAULT RECOMMENDED
-    html = render_to_string('home.html', {'title': title, 'stat': w.detailed_status, 'temp': w.temperature('fahrenheit').get('temp'), 'humid': w.humidity, 'wind': round(w.wind('miles_hour').get('speed'),1),
-    'toprec': 'REC', 'bottomrec': 'REC', 'footwearrec': 'REC', 'layersrec': 'REC'})
+    html = render_to_string('home.html', {'title': title, 'stat': w.detailed_status, 'temp': w.temperature('fahrenheit').get('temp'), 'humid': w.humidity, 'wind': round(w.wind('miles_hour').get('speed'),1)})
     return HttpResponse(html)
 
 def about(request):
@@ -27,21 +28,28 @@ def about(request):
 
 @csrf_exempt
 def profile(request):
+    current_user=request.user
+    user=current_user.email
     if request.method=='POST':
         top = request.POST['top']
         bottom = request.POST['bottom']
         footwear = request.POST['footwear']
-        layers = request.POST['layers']
+        rec_id = request.POST['rec_id']
         #INPUT INTO DATABASE(IF FEEDBACK ALREADY EXISTS FOR THAT DAY, JUST UPDATE)
+        updated_r = Recommendation.objects.get(id=rec_id)
+        updated_r.rec = {"top": top, "bottom": bottom, "footwear": footwear}
+        updated_r.save()
     title='Wearever'
-    current_user=request.user
-    user=current_user.email
     owm = OWM('47005e466640dd018e9b1e79039f299c')
     mgr = owm.weather_manager()
     observation = mgr.weather_at_place('Stony Brook, US')
     w = observation.weather
+    temp = w.temperature('fahrenheit').get('temp')
+    humid = w.humidity
+    wind = round(w.wind('miles_hour').get('speed'),1)
     #GET RECOMMENDATIONS FROM MODEL AND PUT INTO HTML
-    html = render_to_string('profile.html', {'title': title, 'user': user, 'stat': w.detailed_status, 'temp': w.temperature('fahrenheit').get('temp'), 'humid': w.humidity, 'wind': round(w.wind('miles_hour').get('speed'),1),
-    'topdefault': 'DEFAULT', 'bottomdefault': 'DEFAULT', 'footweardefault': 'DEFAULT', 'layersdefault': 'DEFAULT',
-    'toprec': 'REC', 'bottomrec': 'REC', 'footwearrec': 'REC', 'layersrec': 'REC'})
+    recs = get_rec(user, temp, humid, wind)
+    html = render_to_string('profile.html', {'title': title, 'user': user, 'stat': w.detailed_status, 'temp': temp, 'humid': humid, 'wind': wind,
+    'topdefault': recs[1].rec["top"], 'bottomdefault': recs[1].rec["bottom"], 'footweardefault': recs[1].rec["footwear"],
+    'rec_id': recs[1].id, 'toprec': recs[0].rec["top"], 'bottomrec': recs[0].rec["bottom"], 'footwearrec': recs[0].rec["footwear"], 'date': recs[1].date})
     return HttpResponse(html)
